58b42cf493b755990a93c1a4a9064fa11fe746aa
ayylol <davidjgarciao@gmail.com>
2021-12-13 20:10:18 -0700

    Implemented Can mesh for player, changed some values

--- Player.gd
+++ Player.gd
@@ -2,18 +2,14 @@ extends RigidBody
 
 #export var push_cooldown = 1.5
 export var max_pushes = 4
-export var meter_fill_multipliyer = 2.0
-export var pressure_fill_multipliyer = 1.0
+export var meter_fill_multipliyer = 5.0
+export var pressure_fill_multipliyer = 2.5
 
-var max_strength = 25
+var max_strength = 15
 
 var _current_pushed = 0.0
 var _meter_percent_fill = 0.0
 var _held = false
-
-#func _ready():
-#	pass
-	#$Timer.start(push_cooldown)
 
 func _process(delta):
 	_meter_percent_fill= min(_meter_percent_fill+delta/meter_fill_multipliyer, 1.0)
@@ -35,13 +31,6 @@ func release(direction: Vector3):
 		apply_central_impulse (direction*max_strength*_current_pushed)
 		_meter_percent_fill-=_current_pushed
 		_current_pushed=0.0
-#		_current_pushes-=1
-#		if($Timer.is_stopped()):
-#			$Timer.start(push_cooldown)
 
-#func _on_Timer_timeout():
-#	if _current_pushes<max_pushes:
-#		_current_pushes+=1
-#		if _current_pushes!=max_pushes:
-#			$Timer.start(push_cooldown)
-#	print(_current_pushes)
+func compress(direction: Vector3):
+	pass
